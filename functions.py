ed0788467b55b3e8e80d190ee58802c21e22d71d
lidiamayor <lidiamayor2000@gmail.com>
2024-09-04 17:04:29 +0200

    part1

---
+++ functions.py
@@ -0,0 +1,18 @@
+
+
+def clean_state(df):
+    # We can delete the states that have less than 16 attack
+    df = df[df['State'].map(df['State'].value_counts()) > 15]
+    return df
+
+
+def clean_country(df, value):
+    # We can delete the countries that have less than 13 attack
+    df = df[df['Country'].map(df['Country'].value_counts()) > value]
+    return df
+
+
+def clean_sex(df, sub):
+    changes = {' M': 'M', 'M x 2': 'M','M ': 'M', 'lli': 'F'}
+    df[sub] = df[sub].replace(changes)
+    return df
