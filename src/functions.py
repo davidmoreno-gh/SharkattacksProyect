5401e2acc755c9afa7fbaa7a8671395cd19c17f3
David Moreno <124578675+davidmoreno-gh@users.noreply.github.com>
2024-09-05 09:43:32 +0200

    Eliminacion de duplicados en las funciones

--- src/functions.py
+++ src/functions.py
@@ -35,30 +35,12 @@ def clean_country(df, value):
     df = df[df['country'].map(df['country'].value_counts()) > value]
     return df
 
-
 def clean_sex(df, sub):
     changes = {' M': 'M', 'M x 2': 'M','M ': 'M', 'lli': 'F'}
     df[sub] = df[sub].replace(changes)
     return df
 
-############## FUNCIONES DE LYDIA #############
-
-def clean_state(df, value):
-    # We can delete the states that have less than 16 attack
-    df = df[df['State'].map(df['State'].value_counts()) > value]
-    return df
-
-
-def clean_country(df, value):
-    # We can delete the countries that have less than 13 attack
-    df = df[df['Country'].map(df['Country'].value_counts()) > value]
-    return df
-
-
-def clean_sex(df, sub):
-    changes = {' M': 'M', 'M x 2': 'M','M ': 'M', 'lli': 'F'}
-    df[sub] = df[sub].replace(changes)
-    return df
+############## FUNCIONES DE LYDIA ############# (estaban repetidas)
 
 ################# FUNCIONES DE DAVID ####################
 
