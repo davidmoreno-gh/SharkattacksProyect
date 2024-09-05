876dd7dbfc30af4196cf41c740a25bcd6fee2682
lidiamayor <lidiamayor2000@gmail.com>
2024-09-05 23:10:11 +0200

    Ending2

--- src/functions.py
+++ src/functions.py
@@ -1,123 +1,74 @@
-import re
-import pandas as pd
-import numpy as np
-
-
-def clean_labels(data):
-    '''Esta función coge cada nombre de columna de nuestro dataframe, y aplica los siguientes métodos en string:
-    convierte todo en minúscula, remplaza los espacios, puntos y dos puntos por underscores
-    Elimina los posibles espacios del principio y final del string'''
-    data.columns = data.columns.str.lower().str.replace(" ","_").str.replace(".","_").str.replace(":","").str.strip()
-    data.rename(columns={"species_" : "species", "unnamed_11" : "death"}, inplace= True)
-    return data
+def clean_labels(df):
+    '''
+    The function replace the names in lower case, replace spaces, dots and colons
+    Delete the empty spaces at the beginning and at the end of the string
+    Rename the labels
+    '''
+    df.columns = df.columns.str.lower().str.replace(" ","_").str.replace(".","_").str.replace(":","").str.strip()
+    df.rename(columns={"species_" : "species", "unnamed_11" : "death"}, inplace= True)
+    return df
 
 def clean_rows(df):
+    '''
+    The function delete the rows with more than 4000 values to keep the data since 1969
+    '''
     df = df.loc[:4000]
     return df
 
-
-# colocar segundo argumento la lista. juntar con la de abajo
-def clean_corrupted_col(data):
-    useless_cols_to_drop = ['age', 'time', 'species', 'unnamed_21', 'unnamed_22']
-    data = data.drop(columns = useless_cols_to_drop)
-    return data
-
-'''Esta función agrupa las columnas que hemos visto con más de un 60% de datos nulos y las borra'''
-
-
-def clean_unwilling_col(data):
-    unwilling_cols_to_drop = ['date','type','name','injury']
-    data = data.drop(columns = unwilling_cols_to_drop)
-    return data
-
 def drop_cols(df, cols_to_drop):
+    '''
+    Delete de colums indicated in cols_to_drop
+    '''
     df = df.drop(columns=cols_to_drop)
     return df
-    
 
-'''
-def eliminar_columnas_input(data):
-
-    print("Columnas disponibles:")
-    print(data.columns.tolist())
-    
-    selected_to_delete = input("Introduce las columnas que quieras eliminar (separadas por comas): ")
-    selected_to_delete = [col.strip() for col in selected_to_delete.split(',')]
-    
-    columnas_no_existentes = [col for col in selected_to_delete if col not in data.columns]
-    if columnas_no_existentes:
-        print(f"Las siguientes columnas no existen en el DataFrame: {', '.join(columnas_no_existentes)}")
-    
-    data_user_clean = data.drop(columns=[col for col in selected_to_delete if col in data.columns])
-    
-    return data_user_clean
-'''
-
-
-def clean_country(data, value):
-    data = data[data['country'].map(data['country'].value_counts()) > value]
-    return data
-
-
-
-def clean_state(data, value):
-    data = data[data['state'].map(data['state'].value_counts()) > value]
-    return data
-
-
-
-def clean_sex(data):
+def clean_sex(df):
+    '''
+    Changes the sex labels to only M and F
+    '''
     changes = {' M': 'M', 'M x 2': 'M','M ': 'M', 'lli': 'F'}
-    data['sex'] = data['sex'].replace(changes)
-    return data
+    df['sex'] = df['sex'].replace(changes)
+    return df
+
+def null_values_sex(sex):
+    '''
+    Replace the null values in the column 'sex' with the mode
+    '''
+    sex = sex.fillna(sex.mode()[0])
+    return sex
+    
+def null_values_year(year):
+    '''
+    Replace the null values in the column 'year' with the previous value
+    '''
+    year = year.fillna(method ='bfill')
+    return year
+
+def filter_country(df, value):
+    ''' 
+    Delete the rows with a country that appears less than `value` times
+    ''' 
+    df = df[df['country'].map(df['country'].value_counts()) > value]
+    return df
 
 
+def filter_state(df, value):
+    '''
+    Delete the rows with a state that appears less than `value` times
+    '''
+    df = df[df['state'].map(df['state'].value_counts()) > value]
+    return df
 
-################# FUNCIONES DE DAVID ####################
+def filter_year(df):
+    '''
+    Delete the rows with year = 2024 to have a better analysis
+    '''
+    df = df.loc[df['year'] != 2024]
+    return df
 
-'''def BC(string):
-    """Mira en la cadena que le pasemos (pensado para el contenido de df.Date) nos va a devolver 0 
-    en caso de que encuentre dentro de la cadena algo del tipo BC ó bc, para luego esos CEROS, convertirlos a null
-    en nuestro df sobre el que vamos a graficar """
-    try:
-        x = re.search(r'(?i)B\.C', str(string))
-        if x == []: #No hay match dentro de la cadena ("Date" de la fila en la que estemos)
-            return string
-        else:
-            return np.nan #Si encuentra un Bc lo convierte en NaN
-    except IndexError:
-        return string'''
-
-def interval_years(string):
-    """Mira en la cadena que le damos si corresponden con stirngs de datos del tipo YYYY - YYYY o del tipo
-    YYYY and YYYY, para poder hacer la media de este intervalo y ponerlo en la celda df.Year de esa fila"""
-    try:
-        y = re.findall('^\d{4}-\d{4}', str(string)) 
-        if y != None:
-            return int((int(y[0][:4])+int(y[0][5:]))//2) #Media del entero de los 4 primeros digitos + 4 siguientes
-        else:
-            return string
-    except IndexError:
-        return string
-
-def sueltos (string): #4 Digitos aislados
-    """Miramos los año sueltos que estan puesto solo como texto y un solo año"""
-    try:
-        z = re.findall("\d{4}", str(string))
-        if z != None:
-            return z[0]
-        else:
-            return 0
-    except IndexError:
-        return 0
-
-def rescatar_fechas(string): #Clean column
-    """A través de esta funcion aplicamos funcion BC y funcion interval_years en ese orden a lo largo de la string
-    que le metamos, en principio elementos de la columna df.Date"""
-    #string = BC(string)
-    string = sueltos(string)
-    string = interval_years(string)
-    if int(string) == 0:
-         return np.nan
-    else:
-        return string
+def filter_death(df):
+    '''
+    Delete the rows whose values in the column 'death' are not 'Y' or 'N'
+    '''
+    df = df.loc[(df['death']=='Y') | (df['death']=='N')]
+    return df
